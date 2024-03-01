5823a53fb1efe816795529ca98ca6614fb52e9ad
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-03-01 23:03:49 +0100

    Refactoring

--- Server/Data/CarUnitOfWork.cs
+++ Server/Data/CarUnitOfWork.cs
@@ -1,29 +1,31 @@
 ﻿using Server.Data;
 using Server.DAL;
+using Microsoft.EntityFrameworkCore;
 
 namespace Server.Data
 {
-	public class CarUnitOfWork:ICarUnitOfWork
+  public class CarUnitOfWork : ICarUnitOfWork
+  {
+    readonly CarApiContext _context;
+
+    public CarUnitOfWork(CarApiContext context)
     {
-	    readonly CarApiContext _context;
-
-	    public CarUnitOfWork(CarApiContext context)
-	    {
-		    _context = context;
-		    Cars = new CarRepository(_context);
-		    Companies = new CompanyRepository(_context);
-		}
-
-	    public void Dispose()
-	    {
-		   _context.Dispose();
-	    }
-
-	    public ICarRepository Cars { get; private set; }
-	    public ICompanyRepository Companies { get; private set; }
-		public int Complete()
-		{
-			return _context.SaveChanges();
-		}
+      _context = context;
+      Cars = new CarRepository(_context);
+      Companies = new CompanyRepository(_context);
     }
+
+    public void Dispose()
+    {
+      _context.Dispose();
+    }
+
+    public ICarRepository Cars { get; private set; }
+    public ICompanyRepository Companies { get; private set; }
+    
+    public int Complete()
+    {
+      return _context.SaveChanges();
+    }
+  }
 }
