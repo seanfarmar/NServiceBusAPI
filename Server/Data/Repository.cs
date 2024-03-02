c4c487de6b27ca6f8fd7718b5dd8a7f8eb962010
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-03-02 20:10:50 +0100

    nicer

--- Server/Data/Repository.cs
+++ Server/Data/Repository.cs
@@ -6,52 +6,52 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Server.Data
 {
-    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+  public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+  {
+    protected readonly DbContext Context;
+
+    protected Repository(DbContext context)
     {
-	    protected readonly DbContext Context;
+      Context = context;
+    }
 
-	    protected Repository(DbContext context)
-	    {
-		    Context = context;
-		}
+    public void Add(TEntity entity)
+    {
+      Context.Add(entity);
+    }
 
-		public void Add(TEntity entity)
-		{
-			Context.Add(entity);
-		}
+    public void Update(TEntity entity)
+    {
+      Context.Update(entity);
+    }
 
-	    public void Update(TEntity entity)
-	    {
-			Context.Update(entity);
-	    }
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+      Context.AddRange(entities);
+    }
 
-		public void AddRange(IEnumerable<TEntity> entities)
-		{
-			Context.AddRange(entities);
-		}
+    public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+    {
+      return Context.Set<TEntity>().Where(predicate);
+    }
 
-		public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
-		{
-			return Context.Set<TEntity>().Where(predicate);
-		}
+    public TEntity Get(Guid id)
+    {
+      return Context.Set<TEntity>().Find(id);
+    }
+    public IEnumerable<TEntity> GetAll()
+    {
+      return Context.Set<TEntity>().ToList();
+    }
 
-		public TEntity Get(Guid id)
-	    {
-			return Context.Set<TEntity>().Find(id);
-	    }
-	    public IEnumerable<TEntity> GetAll()
-	    {
-		    return Context.Set<TEntity>().ToList();
-	    }
+    public void Remove(TEntity entity)
+    {
+      Context.Remove(entity);
+    }
 
-		public void Remove(TEntity entity)
-		{
-			Context.Remove(entity);
-		}
-
-		public void RemoveRange(IEnumerable<TEntity> entities)
-		{
-			Context.RemoveRange(entities);
-		}
-	}
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+      Context.RemoveRange(entities);
+    }
+  }
 }
